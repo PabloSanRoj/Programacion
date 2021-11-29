f8389d61b2cd3671ba9bfade493836f44a84cce6
Pablo Sanchez <pablo.sanchez.fp@iescampanillas.com>
2021-11-29 12:49:46 +0100

    Update File

--- Relacion7/Ejercicio8Tema7_2.java
+++ Relacion7/Ejercicio8Tema7_2.java
@@ -6,40 +6,22 @@ se indican del 1 al 8.*/
 
 public class Ejercicio8Tema7_2 {
   public static void main(String[] args) {
-    
-    final int VACIO = 0;
-    final int POSICION = 1;
-    final int INTENTO = 3;
 
-    int x;
-    int y;
-    int[][] cuadrante = new int[9][9];
-    // inicializa el array
-    for (x = 1; x < 9; x++) {
-      for (y = 1; y < 9; y++) {
-        cuadrante[x][y] = VACIO;
+    int alfil, fila, columna;
+
+    int [][] comprobador = new int [8][8];
+
+    System.out.print("Introduzca la coordenada del alfil: ");
+    alfil = Integer.parseInt(System.console().readLine());
+
+    fila = alfil % 10;
+    fila /= 10;
+    columna = alfil % 10;
+
+    for (int i = 0; i < 8 ; i++){
+      for (int j = 0 ; j < 8 ; j++){
+        if ((i /= fila && j /= columna))
       }
     }
-    // coloca el alfil
-    int posicionX = Integer.parseInt(System.console().readLine());
-    int posicionY = Integer.parseInt(System.console().readLine());
-    cuadrante[posicionX][posicionY] = POSICION;
-
-    for (y = 7; y >= 0; y--) {
-      System.out.print(y + "|");
-      for (x = 0; x < 8; x++) {
-        if (cuadrante[x][y] == INTENTO) {
-          System.out.print("X ");
-        } else {
-          System.out.print(" ");
-        }
-      }
-      System.out.println();
-    }
-    System.out.println(" ----------------\n 1 2 3 4 5 6 7 8 \n");
-    
-    // juego
-    System.out.println("Movimientos de un alfil");
-
   }
 }
