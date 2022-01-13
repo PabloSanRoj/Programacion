3e1db0ee116a147cbb91a3e6eb5bc8bed6726c3f
Pablo Sanchez <pablo.sanchez.fp@iescampanillas.com>
2022-01-13 12:13:24 +0100

    Update Files

--- Relacion8/src/App.java
+++ Relacion8/src/App.java
@@ -2,12 +2,120 @@ import paquetes.Ejercicio1_14;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(Ejercicio1_14.voltea(1234));
-        System.out.println(Ejercicio1_14.potencia(2, 4));
-        System.out.println(Ejercicio1_14.digitos(78364));
-        System.out.println(Ejercicio1_14.esPrimo(9));
-        System.out.println(Ejercicio1_14.siguientePrimo(20));
-        System.out.println(Ejercicio1_14.esCapicua(200));
+
+        String salir = "no";
+
+        do{
+
+        System.out.println();
+        System.out.println("Menu de opciones funciones parte 1: ");
+        System.out.println("1. Ejercicio esCapicua.");
+        System.out.println("2. Ejercicio esPrimo.");
+        System.out.println("3. Ejercicio siguientePrimo.");
+        System.out.println("4. Ejercicio potencia.");
+        System.out.println("5. Ejercicio digitos.");
+        System.out.println("6. Ejercicio voltea.");
+        System.out.println("7. Ejercicio digitoN.");
+        System.out.println("8. Ejercicio posicionDeDigito.");
+        System.out.println("9. Ejercicio quitaPorDetras.");
+        System.out.println("10. Ejercicio quitaPorDelante.");
+        System.out.println("11. Ejercicio pegaPorDetras.");
+        System.out.println("12. Ejercicio pegaPorDelante.");
+        System.out.println("13. Ejercicio trozoNumero.");
+        System.out.println("14. Ejercicio juntaNumeros.");
+        
+        System.out.println();
+        System.out.print("Selecciona una opcion: ");
+        int selec = Integer.parseInt(System.console().readLine());
+        
+        System.out.println();
+
+        switch (selec){
+            case 1:
+                System.out.println("Introduzca un numero para comprobar si es capicua: ");
+                int num = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.esCapicua(num));
+            break;
+            case 2:
+                System.out.println("Introduzca un numero para comprobar si es Primo: ");
+                int num2 = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.esPrimo(num2));
+            break;
+            case 3:
+                System.out.println("Introduzca un numero para ver cual es el siguiente Primo: ");
+                int num3 = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.siguientePrimo(num3));
+            break;
+            case 4:
+                System.out.println("Introduzca una base y un exponente para ver la potencia: ");
+                int base = Integer.parseInt(System.console().readLine());
+                int exponente = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.potencia(base, exponente));
+            break;
+            case 5:
+                System.out.println("Introduzca un numero para ver cuantos digitos tiene: ");
+                long num5 = Long.parseLong(System.console().readLine());
+                System.out.println(Ejercicio1_14.digitos(num5));
+            break;
+            case 6:
+                System.out.println("Introduzca un numero para ponerlo al reves: ");
+                long num6 = Long.parseLong(System.console().readLine());
+                System.out.println(Ejercicio1_14.voltea(num6));
+            break;
+            case 7:
+                System.out.println("Introduzca un numero y una posicion y te devolvera el digito de esa posicion: ");
+                long num7 = Long.parseLong(System.console().readLine());
+                int posi = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.digitoN(num7, posi));
+            break;
+            case 8:
+                System.out.println("Introduzca un numero y un digito y te devolvera la posicion el la que se encuentra, si no esta devuelve -1: ");
+                long num8 = Long.parseLong(System.console().readLine());
+                long ocu = Long.parseLong(System.console().readLine());
+                System.out.println(Ejercicio1_14.posiciondeDigito(num8, ocu));
+            break;
+            case 9:
+                System.out.println("Introduzca un numero y cuantos digitos quieres quitarle por la izquierda: ");
+                long num9 = Long.parseLong(System.console().readLine());
+                int quita = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.quitaPorDelante(num9, quita));
+            break;
+            case 10:
+                System.out.println("Introduzca un numero y cuantos digitos quieres quitarle por la derecha: ");
+                long num10 = Long.parseLong(System.console().readLine());
+                int quita2 = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.quitaPorDetras(num10, quita2));
+            break;
+            case 11:
+                System.out.println("Introduzca un numero y el digito que quieres pegarle por la derecha: ");
+                long num11 = Long.parseLong(System.console().readLine());
+                long pega = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.pegaPorDetras(num11, pega));
+            break;
+            case 12:
+                System.out.println("Introduzca un numero y el digito que quieres pegarle por la izquierda: ");
+                long num12 = Long.parseLong(System.console().readLine());
+                long pega2 = Integer.parseInt(System.console().readLine());
+                System.out.println(Ejercicio1_14.pegaPorDelante(num12, pega2));
+            break;
+            case 13:
+                System.out.println("Introduzca un numero y te juntara el primer y ultimo dígito: ");
+                long num13 = Long.parseLong(System.console().readLine());
+                System.out.println(Ejercicio1_14.trozoDeNumero(num13));
+            break;
+            case 14:
+                System.out.println("Introduzca 2 numeros y te los juntara: ");
+                long num14 = Long.parseLong(System.console().readLine());
+                long pega3 = Long.parseLong(System.console().readLine());
+                System.out.println(Ejercicio1_14.juntaNumeros(num14, pega3));
+            break;
+            default:
+        }
+
+        System.out.println("¿Desea Salir del Menu? (si/no)");
+        salir = System.console().readLine();
+
+        } while (salir.equalsIgnoreCase("no"));
     }
     
 }
--- Relacion8/src/paquetes/Ejercicio1_14.java
+++ Relacion8/src/paquetes/Ejercicio1_14.java
@@ -24,11 +24,11 @@ public class Ejercicio1_14 {
     return j;
   }
 
-  public static long digitos(int numero) {
+  public static int digitos(long numero) {
 
     int numeroDigi = 0;
 
-    for (int i = numero; i > 0; i /= 10) {
+    for (long i = numero; i > 0; i /= 10) {
       numeroDigi++;
     }
 
@@ -64,9 +64,139 @@ public class Ejercicio1_14 {
 
   }
 
-  public static long digitoN(int numero) {
+  public static long digitoN(long numero, int numQuita) {
+
+    long num;
+    int numdigi;
+
+    
+      num = quitaPorDelante(numero, numQuita);
+    
+      numdigi = digitos(num);
+      numQuita = numdigi - 1;
+
+      num = quitaPorDetras(num, numQuita);
+
+    return num;
+  }
+
+  public static long quitaPorDelante(long numero, int numQuita) {
+
+    return numero % potencia(10, digitos(numero) - numQuita);
+
+  }
+
+  public static long quitaPorDetras(long numero, int numQuita) {
+
+    for (long i = numQuita ; i > 0 ; i--){
+      numero /= 10;
+    }
 
     return numero;
+
+  }
+
+  public static long pegaPorDetras(long numero, long numeroPega) {
+
+    int numDigi;
+    long num;
+
+    numDigi = digitos(numeroPega);
+
+    for (int i = numDigi ; i > 0 ; i--){
+
+    num = quitaPorDetras(numeroPega, numDigi - 1);
+    numero *= 10;
+    numero = numero + num;
+    numDigi--;
+    numeroPega = numeroPega % potencia(10, digitos(numeroPega) - 1);
+    }
+    return numero;
+
+  }
+
+  public static long posiciondeDigito(long numero, long ocurrencia) {
+
+    boolean salir = false;
+    long num, contador = 0, cambiar = 0;
+    long posicion = 0;;
+
+    do{
+
+      for (long i = numero ; i > 0 ; i /= 10){
+        num = i % 10;
+
+        if (num == ocurrencia){
+          posicion = contador;
+          cambiar = 1;
+          salir = true;
+        }
+        contador++;
+      }
+      salir = true;
+      
+    } while (salir == false);
+    
+    if (cambiar == 0){
+      posicion = -1;
+    }
+
+    return posicion;
+
+  }
+
+  public static long trozoDeNumero(long numero) {
+
+    long num, num2; 
+    int numDigi;
+
+    numDigi = digitos(numero);
+
+    num = quitaPorDelante(numero, numDigi - 1);
+
+    num2 = quitaPorDetras(numero, numDigi - 1);
+
+    numero = (num2 * 10) + num;
+
+    return numero;
+
+  }
+
+  public static long juntaNumeros(long numero, long numeroPega) {
+
+    int numDigi;
+    long num;
+
+    numDigi = digitos(numeroPega);
+
+    for (int i = numDigi ; i > 0 ; i--){
+
+    num = quitaPorDetras(numeroPega, numDigi - 1);
+    numero *= 10;
+    numero = numero + num;
+    numDigi--;
+    numeroPega = numeroPega % potencia(10, digitos(numeroPega) - 1);
+    }
+    return numero;
+
+  }
+
+  public static long pegaPorDelante(long numero, long numeroPega) {
+
+    int numDigi;
+    long num;
+
+    numDigi = digitos(numero);
+
+    for (int i = numDigi ; i > 0 ; i--){
+
+    num = quitaPorDetras(numero, numDigi - 1);
+    numeroPega *= 10;
+    numeroPega = numeroPega + num;
+    numDigi--;
+    numero = numero % potencia(10, digitos(numero) - 1);
+    }
+    return numeroPega;
 
   }
 }
